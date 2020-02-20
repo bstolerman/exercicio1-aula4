26810b38fbb983394b884af36328166926e08768
Luana <luanadfortiz@gmail.com>
2020-02-20 12:01:17 -0300

    criar exercicio 1

---
+++ funcoes.py
@@ -0,0 +1,18 @@
+def somar_dois_valores(a, b):
+    resultado = a + b
+
+    if (resultado > 40):
+        print('O valor é: ', resultado)
+    else:
+        print('Ops, só retorno valores acima de 40')
+
+    return resultado
+
+# variavel = chamada de funcao(argumento 1, argumento2)
+a_input = int(input('Digite o valor a: '))
+b_input = int(input('Digite o valor b: '))
+
+somar_dois_valores(a_input, b_input)
+
+
+
